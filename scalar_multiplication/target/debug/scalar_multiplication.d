884ef94e004dc0f4d73ad24b7ace42368a166eea
kenkomu <komukenneth216@gmail.com>
2025-02-17 10:56:15 +0300

    order of point

--- scalar_multiplication/target/debug/scalar_multiplication.d
+++
@@ -1,1 +0,0 @@
-/home/ken/Projects/elliptic_curve_cryptography/scalar_multiplication/target/debug/scalar_multiplication: /home/ken/Projects/elliptic_curve_cryptography/scalar_multiplication/src/main.rs
